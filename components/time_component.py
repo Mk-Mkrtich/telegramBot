0bb92a060be8d1e3bb7a6dc13361f7dc7803241a
Mkrtich Mkrtchyan <mkrtich@bl.ink>
2024-08-20 17:32:03 +0400

    added ban times

--- components/time_component.py
+++ components/time_component.py
@@ -1,13 +1,23 @@
 from telebot import types
 from telebot.types import InlineKeyboardButton
+from configs.storage import can_not
+import datetime
 
 
 def generate_time_buttons():
+    current_time = datetime.datetime.now().hour
     markup = types.InlineKeyboardMarkup()
     row = []
     for hour in range(24):
         time_str = f"{hour:02}:00"
-        row.append(InlineKeyboardButton(time_str, callback_data="fideTime_" + str(time_str)))
+        callback_data = ""
+        if hour > current_time:
+            callback_data += "fideTime_" + str(time_str)
+            text = time_str
+        else:
+            callback_data += "ignore"
+            text = can_not + " " + time_str
+        row.append(InlineKeyboardButton(text, callback_data=callback_data))
         if (hour + 1) % 4 == 0:
             markup.row(*row)
             row = []
