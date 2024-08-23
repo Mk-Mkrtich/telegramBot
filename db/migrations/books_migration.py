dc2cefaf387ceb9bdfc343b21623f66baeb20946
Mkrtich Mkrtchyan <mkrtich@bl.ink>
2024-08-23 17:36:12 +0400

    comment migrations

--- db/migrations/books_migration.py
+++ db/migrations/books_migration.py
@@ -1,10 +1,10 @@
-def create_books_table():
-    return """
-        CREATE TABLE IF NOT EXISTS books (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            ride_id INT NOT NULL,
-            booked_places INT NOT NULL,
-            passenger_name VARCHAR(255) NULL,
-            passenger_id INT NULL
-        )
-        """
+# def create_books_table():
+#     return """
+#         CREATE TABLE IF NOT EXISTS books (
+#             id INT AUTO_INCREMENT PRIMARY KEY,
+#             ride_id INT NOT NULL,
+#             booked_places INT NOT NULL,
+#             passenger_name VARCHAR(255) NULL,
+#             passenger_id INT NULL
+#         )
+#         """
--- db/migrations/rides_migration.py
+++ db/migrations/rides_migration.py
@@ -1,18 +1,18 @@
-def create_rides_table():
-    return """
-        CREATE TABLE IF NOT EXISTS rides (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            user_name VARCHAR(255) NULL,
-            user_id INT NULL,
-            from_city VARCHAR(255) NOT NULL,
-            to_city VARCHAR(255) NOT NULL,
-            ride_date DATE NOT NULL,
-            ride_time VARCHAR(255) NOT NULL,
-            places INT NOT NULL,
-            free_places INT NOT NULL,
-            price DECIMAL(10, 2) NOT NULL,
-            car_mark VARCHAR(255) NOT NULL,
-            car_number VARCHAR(255) NOT NULL,
-            car_color VARCHAR(255) NOT NULL
-        )
-        """
+# def create_rides_table():
+#     return """
+#         CREATE TABLE IF NOT EXISTS rides (
+#             id INT AUTO_INCREMENT PRIMARY KEY,
+#             user_name VARCHAR(255) NULL,
+#             user_id INT NULL,
+#             from_city VARCHAR(255) NOT NULL,
+#             to_city VARCHAR(255) NOT NULL,
+#             ride_date DATE NOT NULL,
+#             ride_time VARCHAR(255) NOT NULL,
+#             places INT NOT NULL,
+#             free_places INT NOT NULL,
+#             price DECIMAL(10, 2) NOT NULL,
+#             car_mark VARCHAR(255) NOT NULL,
+#             car_number VARCHAR(255) NOT NULL,
+#             car_color VARCHAR(255) NOT NULL
+#         )
+#         """
